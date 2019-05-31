e98bd617adf59340a20754303c49ddcd47084b6f
JerYoMat <jeremy.emata@gmail.com>
2019-05-31 14:31:41 -0400

    jwt infrastructure -app/auth handles actual authentication -app/lib defines JWT and message -app/controllers/concerns defines response and exception handling -other: application_controller currently requires every request to authenticate, will likely need to loosen this constraint once it is clear how much will b epublicly available

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope :api do 
     scope :v1 do
       resources :users, only: [:create, :update, :destroy]
+      post   'auth/login',   to: 'authentication#authenticate'
     end       
   end 
 end
